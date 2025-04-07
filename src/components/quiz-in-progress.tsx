521a460c0f93be7aafdb316bd67636d74129b4f0
Moscolape <chunkums088@gmail.com>
2025-04-07 08:08:20 +0100

    chore: correct 60mins quiz to 10mins

--- src/components/quiz-in-progress.tsx
+++ src/components/quiz-in-progress.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PageWrapper from "./pageWrapper";
 
-const QUIZ_DURATION = 60 * 60 * 1000;
+const QUIZ_DURATION = 10 * 60 * 1000;
 
 type Question = {
   _id: string;
