a5d928127d8d67e951fb4c3aed9dce274d5f4964
Moscolape <chunkums088@gmail.com>
2025-05-03 18:35:05 +0100

    chore: fetch today's quiz questions

--- src/components/quiz-in-progress.tsx
+++ src/components/quiz-in-progress.tsx
@@ -94,7 +94,7 @@ const QuizInProgress = () => {
       setLoading(true);
       try {
         const response = await fetch(
-          "https://best-brain-contest-backend.onrender.com/api/admin/questions?day=2025-04-26"
+          "https://best-brain-contest-backend.onrender.com/api/admin/questions?day=2025-05-03"
         );
         const data = await response.json();
 
